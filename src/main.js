a9ac9dc8c190be720cbb307cd5f50c94e79b0c09
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-08-21 12:59:23 +0200

    Moved commit and push logic to main step

--- src/main.js
+++ src/main.js
@@ -7,3 +7,9 @@ if (repoType === 'element' || repoType === 'lambda') {
   spawnSync('git', ['add', '*.js'], { stdio: 'inherit' })
 }
 console.log('SUCCESS: successfully initialized repository!')
+console.log('INFO: commiting and pushing changes...')
+const gpgSign = spawnSync('git', ['config', 'commit.gpgSign']).stdout
+const commitParams = gpgSign.length > 0 ? ['commit', '-S', '-am', 'Initialized repository with correct naming'] : ['commit', '-am', 'Initialized repository with correct naming']
+spawnSync('git', commitParams, { stdio: 'inherit' })
+spawnSync('git', ['push'], { stdio: 'inherit' })
+console.log('SUCCESS: commited and pushed changes!')
