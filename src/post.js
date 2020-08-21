fbfb9ede998fc4598925da2d3e66a67603c795f3
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-08-21 12:53:07 +0200

    Fixing conditional signing

--- src/post.js
+++ src/post.js
@@ -8,7 +8,7 @@ console.log('INFO: removing init workflow from repository...')
 spawnSync('git', ['rm', '.github/workflows/init.yml'], { stdio: 'inherit' })
 console.log('SUCCESS: successfully removed workflow!')
 console.log('INFO: commiting and pushing changes...')
-const gpgSign = spawnSync('git', ['config', 'commit.gpgSign'], { stdio: 'inherit' }).stdout
+const gpgSign = spawnSync('git', ['config', 'commit.gpgSign']).stdout
 const commitParams = gpgSign.length > 0 ? ['commit', '-S', '-am', 'Initialized repository with correct naming'] : ['commit', '-am', 'Initialized repository with correct naming']
 spawnSync('git', commitParams, { stdio: 'inherit' })
 spawnSync('git', ['push'], { stdio: 'inherit' })
