90c6b97c3c8624c8369ef135b622c813d8b2a4d0
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-08-21 12:16:14 +0200

    Integrated dependencies install into pre step

--- dist/pre.js
+++ dist/pre.js
@@ -1,4 +1,15 @@
 const { spawnSync } = require('child_process')
+if (process.env.GITHUB_ACTIONS && process.env.GITHUB_REPOSITORY !== 'kaskadi/action-init') {
+  const callingRepo = process.cwd()
+  process.chdir(__dirname)
+  process.chdir('..')
+  console.log('INFO: installing action dependencies...')
+  console.log('************ NPM ouput ************')
+  spawnSync('npm', ['i', '--only=prod'], { stdio: 'inherit' })
+  console.log('************ End of NPM ouput ************')
+  console.log('SUCCESS: dependencies installed!')
+  process.chdir(callingRepo)
+}
 require('../helpers/install-dependencies.js')(spawnSync)
 console.log('INFO: installing kaskadi-cli in repository...')
 console.log('************ NPM ouput ************')
