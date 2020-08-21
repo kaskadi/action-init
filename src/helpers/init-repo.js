513f427e11825121f83ebc1a88af0efe191bc739
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-08-21 13:30:00 +0200

    Fixed call to CLI

--- src/helpers/init-repo.js
+++ src/helpers/init-repo.js
@@ -1,6 +1,6 @@
 module.exports = (spawnSync, repoType) => {
   console.log(`INFO: initializing repository as ${repoType} repository...`)
-  spawnSync('source', ['node_modules/.bin/kaskadi', 'init', repoType], { stdio: 'inherit' })
+  spawnSync('node', ['node_modules/kaskadi-cli/kaskadi', 'init', repoType], { stdio: 'inherit' })
   if (repoType === 'element' || repoType === 'lambda') {
     spawnSync('git', ['add', '*.js'], { stdio: 'inherit' })
   }
