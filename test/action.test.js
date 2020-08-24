3e777f82a72de5cd7b25238c6db02fd4bd539e20
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-08-24 12:34:54 +0200

    Initialized repository with correct naming

--- test/action.test.js
+++ test/action.test.js
@@ -1,31 +1,27 @@
 /* eslint-env mocha */
-const childProc = require('child_process')
+const runAction = require('./helpers/run-action.js')
+const steps = ['pre', 'main']
 const chai = require('chai')
+const { assert } = require('chai')
 chai.should()
 
 // *************
 // Write your tests here
-describe('action-init', function () {
+describe('template-action', function () {
   // this.timeout(10000)
-
   // uncomment this if you want to raise the timeout cap for your whole test (when you need to perform long async tasks). You can also follow the same logic per describe block or per it block to increase the timeout cap
   // if your describe/it callbacks are arrow functions, you need to use the following syntax: describe('...', () => {}).timeout(...) but be aware that this won't apply to hooks!
-})
-// *************
 
-// awaitable function to execute the main script of this action
-function execMain () {
-  return new Promise((resolve, reject) => {
-    childProc.exec('node index', (err, stdout, stderr) => {
-      if (err === null) {
-        console.log(stdout)
-        resolve(true)
-      } else {
-        console.log(stderr)
-        resolve(false)
-      }
+  // ******* Example tests
+  before(function () {
+    runAction(steps)
+  })
+  describe('Placeholder test', function () {
+    it('should pass', function () {
+      const exp = true
+      assert(exp === true, 'True is true...')
     })
   })
-}
-
-// just add any helpers you may need
+  // *******
+})
+// *************
