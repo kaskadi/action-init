fb41b2d9388d63eaa4525c7678efffeac2bb88b6
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-11-12 16:06:37 +0100

    Removed logging of NPM output (faster runtime)

--- src/helpers/kaskadi-cli-manager.js
+++ src/helpers/kaskadi-cli-manager.js
@@ -2,8 +2,6 @@ module.exports = (spawnSync, op) => {
   const header = `INFO: ${op === 'i' ? 'installing' : 'removing'} kaskadi-cli ${op === 'i' ? 'in' : 'from'} repository...`
   const footer = `SUCCESS: kaskadi-cli ${op === 'i' ? 'installed' : 'removed'}!`
   console.log(header)
-  console.log('************ NPM ouput ************')
-  spawnSync('npm', [op, 'kaskadi-cli'], { stdio: 'inherit' })
-  console.log('************ End of NPM ouput ************')
+  spawnSync('npm', [op, 'kaskadi-cli'])
   console.log(footer)
 }
