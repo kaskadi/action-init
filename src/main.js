b6f2888f9a53f9b52c46ec68981ebbc61230c689
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-11-12 16:05:51 +0100

    Perform git operations only in production env

--- src/main.js
+++ src/main.js
@@ -4,11 +4,14 @@ const kaskadiCLIManager = require('./helpers/kaskadi-cli-manager')
 const initRepo = require('./helpers/init-repo.js')
 const cleanup = require('./helpers/cleanup.js')
 const pushChanges = require('./helpers/push-changes.js')
+
 const repoType = core.getInput('repoType')
-const test = !process.env.GITHUB_ACTIONS || process.env.GITHUB_REPOSITORY === 'kaskadi/action-init'
+const test = process.env.TEST_ENV ? JSON.parse(process.env.TEST_ENV) : false
 
 kaskadiCLIManager(spawnSync, 'i')
 initRepo(spawnSync, repoType)
 kaskadiCLIManager(spawnSync, 'rm')
-cleanup(spawnSync, test)
-pushChanges(spawnSync, test)
+if (!test) {
+  cleanup(spawnSync)
+  pushChanges(spawnSync)
+}
