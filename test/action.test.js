e694f78f9534cb28a876c8e34840720bf52d9a32
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-11-13 19:14:30 +0100

    Updated tests for API/git operation support

--- test/action.test.js
+++ test/action.test.js
@@ -5,26 +5,30 @@ const { spawn } = require('child_process')
 const steps = ['pre', 'main']
 const copyData = require('./helpers/copy-data.js')
 const rimraf = require('rimraf')
+const should = require('chai').should()
+const { readFileSync } = require('fs')
 
 const folderName = 'working-data'
 
 const wd = process.cwd()
 
 const args = {
-  action: ['template-action', folderName]
-  // api: ['template-api', folderName],
-  // element: ['template-element', folderName, 'TemplateElement', 'WorkingData'],
-  // lambda: ['template-lambda', folderName, 'templateLambda', 'workingData'],
-  // layer: ['template-layer', folderName, 'templateLayer', 'workingData'],
-  // package: ['template-npm-pkg', folderName]
+  action: ['template-action', folderName],
+  api: ['template-api', folderName],
+  element: ['template-element', folderName, 'TemplateElement', 'WorkingData'],
+  lambda: ['template-lambda', folderName, 'templateLambda', 'workingData'],
+  layer: ['template-layer', folderName, 'templateLayer', 'workingData'],
+  package: ['template-npm-pkg', folderName]
 }
 
 describe('action-init', function () {
   this.timeout(30000)
   // ******* DO NOT REMOVE THIS TEST!
-  // require('./pre/tests.js')
+  require('./pre/tests.js')
   // *******
   let servProc
+  let servOutput
+  const outputPath = require('path').join(__dirname, 'server-output.json')
   before(done => {
     servProc = spawn('node', ['test/test-server.js'])
     servProc.stdout.on('data', (data) => {
@@ -44,6 +48,35 @@ describe('action-init', function () {
   for (const repoType in args) {
     describe(`${repoType} initialization`, describeHandler(repoType, args[repoType]))
   }
+  describe('missing token', describeHandler('action', args.action, () => {
+    it('should not send requests to GitHub and Code Climate API if no Code Climate token is provided', () => {
+      servOutput = readServerOutput(outputPath)
+      should.not.exist(servOutput.GITHUB_API_CALL)
+      should.not.exist(servOutput.REPO_PUBLIC)
+      should.not.exist(servOutput.CC_API_CALL)
+      process.env.CC_TOKEN = 'abc'
+      process.env.GITHUB_REPOSITORY = 'hello/world'
+    })
+  }))
+  describe('no repo/repo private', describeHandler('action', args.action, () => {
+    it('should send a request to GitHub but not Code Climate if token is defined and repository does not exist/is private', () => {
+      servOutput = readServerOutput(outputPath)
+      servOutput.GITHUB_API_CALL.should.equal(true)
+      servOutput.REPO_PUBLIC.should.equal(false)
+      should.not.exist(servOutput.CC_API_CALL)
+      process.env.GITHUB_REPOSITORY = 'test/test'
+    })
+  }))
+  describe('public repo', describeHandler('action', args.action, () => {
+    it('should send a request to GitHub and Code Climate if token is defined and repository is public', () => {
+      servOutput = readServerOutput(outputPath)
+      servOutput.GITHUB_API_CALL.should.equal(true)
+      servOutput.REPO_PUBLIC.should.equal(true)
+      servOutput.CC_API_CALL.should.equal(true)
+      delete process.env.CC_TOKEN
+      delete process.env.GITHUB_REPOSITORY
+    })
+  }))
   after(done => {
     servProc.on('exit', (code) => {
       done()
@@ -52,7 +85,11 @@ describe('action-init', function () {
   })
 })
 
-function describeHandler (repoType, args, postRunTests = () => {}) {
+function readServerOutput (outputPath) {
+  return JSON.parse(readFileSync(outputPath, 'utf8'))
+}
+
+function describeHandler (repoType, args, postRunTests) {
   const testDir = `test/${repoType}-data`
   const dataPath = `${testDir}/data`
   const workingDataPath = `${testDir}/${folderName}`
@@ -62,10 +99,14 @@ function describeHandler (repoType, args, postRunTests = () => {}) {
       await copyData(dataPath, workingDataPath)
       process.env.INPUT_REPOTYPE = repoType
       process.chdir(workingDataPath)
-      await runAction(steps).then(postRunTests)
+      await runAction(steps)
       process.chdir(wd)
     })
-    require(`./${repoType}-data/tests.js`)(testDir, ...args)
+    if (postRunTests) {
+      postRunTests()
+    } else {
+      require(`./${repoType}-data/tests.js`)(testDir, ...args)
+    }
     after(() => {
       rimraf.sync(workingDataPath)
     })
