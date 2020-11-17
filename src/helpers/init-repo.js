9d972419a2aec998c75256ed024454269b90e422
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-11-17 14:56:59 +0100

    Staging all files on init

--- src/helpers/init-repo.js
+++ src/helpers/init-repo.js
@@ -1,9 +1,8 @@
+const gitOp = require('./git-op.js')
+
 module.exports = (spawnSync, repoType) => {
   console.log(`INFO: initializing repository as ${repoType} repository...`)
   spawnSync('node', ['node_modules/kaskadi-cli/kaskadi', 'init', repoType], { stdio: 'inherit' })
-  if (repoType === 'element' || repoType === 'lambda') {
-    const gitOp = require('./git-op.js')
-    gitOp(spawnSync, 'add', ['*.js'])
-  }
+  gitOp(spawnSync, 'add', ['.'])
   console.log('SUCCESS: successfully initialized repository!')
 }
