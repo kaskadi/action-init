adf7f264bd7719ef78e50b2838a7c93f70180b81
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-11-20 11:48:00 +0100

    Code cleanup

--- src/helpers/add-reporter-id.js
+++ src/helpers/add-reporter-id.js
@@ -6,13 +6,10 @@ const baseInit = {
   }
 }
 
-module.exports = (utils, repo) => repoData => {
-  return getPublicKey(utils, repo).then(addSecret(utils, repo, repoData))
-}
+module.exports = (utils, repo) => repoData => getPublicKey(utils, repo).then(addSecret(utils, repo, repoData))
 
 function getPublicKey ({ fetch, checkStatus }, repo) {
-  const init = { ...baseInit }
-  return fetch(`https://api.github.com/repos/${repo}/actions/secrets/public-key`, init)
+  return fetch(`https://api.github.com/repos/${repo}/actions/secrets/public-key`, { ...baseInit })
     .then(checkStatus(`ERROR: unable to retrieve public key for ${repo}. To make sure this does not come from access right to GitHub API, please provide a GITHUB_TOKEN to this action which has repo scope.`))
     .then(res => res.json())
 }
@@ -20,12 +17,9 @@ function getPublicKey ({ fetch, checkStatus }, repo) {
 function addSecret ({ fetch, checkStatus }, repo, repoData) {
   return pubKey => {
     const sodium = require('tweetsodium')
-    const value = repoData.data.attributes.test_reporter_id
-    // Convert the message and key to Uint8Array's (Buffer implements that interface)
-    const msgBytes = Buffer.from(value)
+    const idBytes = Buffer.from(repoData.data.attributes.test_reporter_id)
     const keyBytes = Buffer.from(pubKey.key, 'base64')
-    // Encrypt using LibSodium.
-    const encryptedBytes = sodium.seal(msgBytes, keyBytes)
+    const encryptedBytes = sodium.seal(idBytes, keyBytes)
     const body = {
       encrypted_value: Buffer.from(encryptedBytes).toString('base64'),
       key_id: pubKey.key_id
@@ -37,6 +31,5 @@ function addSecret ({ fetch, checkStatus }, repo, repoData) {
     }
     return fetch(`https://api.github.com/repos/${repo}/actions/secrets/CC_REPORTER_ID`, init)
       .then(checkStatus(`ERROR: unable to set CC_REPORTER_ID secret for ${repo}. To make sure this does not come from access right to GitHub API, please provide a GITHUB_TOKEN to this action which has repo scope.`))
-      .then(res => res.json())
   }
 }
